987afbfaf0fa8fff5941915af301335983b78e66
Juergen Fickel <juergen.fickel@bosch.io>
2021-09-14 08:27:34 +0200

    Issue #559: Added empty lines to improve readability. Slightly adjusted some Javadoc comments.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- java/src/main/java/org/eclipse/ditto/client/options/OptionName.java
+++ java/src/main/java/org/eclipse/ditto/client/options/OptionName.java
@@ -35,11 +35,13 @@ public interface OptionName extends Predicate<Object> {
      * @since 1.0.0
      */
     enum Global implements OptionName {
+
         /**
          * Name of the option for defining the DittoHeaders to send along with a command/message to the Ditto backend.
          * @since 1.1.0
          */
         DITTO_HEADERS
+
     }
 
     /**
@@ -48,34 +50,44 @@ public interface OptionName extends Predicate<Object> {
      * @since 1.0.0
      */
     enum Modify implements OptionName {
+
         /**
          * Name of the option for defining whether a response for a modifying operation is required or not.
          */
         RESPONSE_REQUIRED,
+
         /**
          * Name of the option for defining whether the object affected by the modifying operation must exist or whether
          * it must not exist.
          * <p>
          * This option is only supported for set/put operations. That means it is <strong>not</strong> supported for all
-         * other operations (retrieve, create, update and delete). In the latter case, an {@link
-         * UnsupportedOperationException} will be thrown.
+         * other operations (retrieve, create, update and delete). In the latter case, an
+         * {@link UnsupportedOperationException} will be thrown.
          * </p>
          * <p>If the option is not specified, it does not matter whether the object exists.
          * </p>
          */
         EXISTS,
+
         /**
-         * Name of the option for defining whether the policy should be copied from another policy when creating a thing.
+         * Name of the option for defining whether the policy should be copied from another policy when creating a
+         * thing.
+         *
          * @since 1.1.0
          */
         COPY_POLICY,
+
         /**
          * Name of the option for defining whether the policy should be copied from another thing when creating a thing.
+         *
          * @since 1.1.0
          */
         COPY_POLICY_FROM_THING,
+
         /**
-         * Name of the option for defining whether the thing should be updated based on the specified condition.
+         * Name of the option for defining whether the thing command should be applied, based on the specified
+         * condition.
+         *
          * @since 2.1.0
          */
         CONDITION
@@ -87,19 +99,23 @@ public interface OptionName extends Predicate<Object> {
      * @since 1.0.0
      */
     enum Consumption implements OptionName {
+
         /**
          * Name of the option for defining for which namespaces (as comma separated list) to start consumption for.
          */
         NAMESPACES,
+
         /**
          * Name of the option for defining which RQL expression / filter should match in the backend for events to be
          * delivered to this client.
          */
         FILTER,
+
         /**
          * Name of the option for selecting extra fields for events to be delivered to this client.
          */
         EXTRA_FIELDS
+
     }
 
 }
